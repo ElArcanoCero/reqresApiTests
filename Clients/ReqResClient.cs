971a25b94a958067cd44a9d11ce6e6188d799d7f
Jose Joaquin Restrepo P <arcano.pluz@gmail.com>
2026-04-04 13:09:56 -0500

    primera implementacion

---
+++ Clients/ReqResClient.cs
@@ -0,0 +1,30 @@
+﻿using reqresApiTests.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace reqresApiTests.Clients
+{
+    public class ReqResClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ReqResClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://reqres.in/api/");
+        }
+
+        public async Task<UserResponse> GetUser(int id)
+        {
+            var response = await _httpClient.GetAsync($"users/{id}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<UserResponse>();
+
+            return result;
+        }
+    }
+}
--- Tests/userTests.cs
+++ Tests/userTests.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using reqresApiTests.Clients;
+using FluentAssertions;
+
 namespace reqresApiTests.Tests
 {
-    internal class userTests
+    public class UserTests
     {
+        [Fact]
+        public async Task GetUser_ShouldReturnValidUser()
+        {
+            var client = new ReqResClient();
+
+            var result = await client.GetUser(2);
+
+            result.Data.Id.Should().Be(2);
+            result.Data.Email.Should().NotBeNullOrEmpty();
+            result.Data.First_Name.Should().NotBeNullOrEmpty();
+        }
     }
 }
